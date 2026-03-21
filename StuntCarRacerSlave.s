3aed4f1beebfce9e50c832aefc85e1b64ddbba42
Vesa Halttunen <vesuri@jormas.com>
2026-03-21 21:11:18 +0100

    Add boot DOS file loading and game data size (slave v2)

--- StuntCarRacerSlave.s
+++ StuntCarRacerSlave.s
@@ -26,6 +26,8 @@ slv_Version	= 16
 slv_Flags	= WHDLF_NoError|WHDLF_Examine
 slv_keyexit	= $59	; F10
 
+gameDataSize	equ	408544
+
 	INCLUDE	"kick31.s"
 
 	dc.b	"$VER: StuntCarRacer.slave 0.x (18.03.2026)",0
@@ -52,7 +54,7 @@ _bootdos:
 	move.l	a3,d1
 	jsr	_LVOLoadSeg(a6)
 	tst.l	d0
-	bne.s	.checkVersion
+	bne.s	.loadData
 
 .readFailure:
 	jsr	_LVOIoErr(a6)
@@ -63,15 +65,28 @@ _bootdos:
 	add.l	#resload_Abort,(sp)
 	rts
 
-.checkVersion:
+.loadData:
 	add.l	d0,d0
 	add.l	d0,d0
 	addq	#4,d0
 	move.l	d0,a4
 
+	move.l	8(a4),a5
+	lea	-4(a5),a0
+	move.l	#$e898,d0
+	move.l	#gameDataSize+4,d1
+	moveq	#1,d2
+	move.l	_resload(pc),a2
+	jsr	resload_DiskLoad(a2)
+
+	move.l	#(gameDataSize+4)>>2,d0
+	move.l	#$c905b365,d5
+	move.l	#$a0cff27b,d6
+	move.l	#$59f3a592,d7
+	lea	-4(a5),a0
+	bsr	_Decrypt
+
 	lea	args(pc),a0
-	move.l	4(sp),d0
-	sub.l	#5*4,d0
 	moveq	#argsEnd-args,d0
 	jsr	(a4)
 
@@ -83,4 +98,20 @@ patchList:
 	PL_START
 	PL_END
 
+_Decrypt	movem.l	d0/d5-d7/a0,-(sp)	;Rob Northen Decryption (3 Key)
+.DecryptLoop	lsl.l	#1,d7
+		btst	d5,d7
+		beq.s	.Skip1
+		btst	d6,d7
+		beq.s	.Skip3
+		bra.s	.Skip2
+.Skip1		btst	d6,d7
+		beq.s	.Skip2
+.Skip3		addq.l	#1,d7			;Modify key for correct btst otherwise fuckup!
+.Skip2		add.l	d7,(a0)			;Modify key to encrypted data = correct data
+		add.l	(a0)+,d7		;Modify key with next encrypted longword
+		subq.l	#1,d0			;Subtract from counter until null
+		bne.s	.DecryptLoop
+		movem.l	(sp)+,d0/d5-d7/a0
+		rts
 	END
