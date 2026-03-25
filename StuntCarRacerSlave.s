190f4256e461052970cbc5e34606c63902f22ed0
Vesa Halttunen <vesuri@jormas.com>
2026-03-25 23:38:01 +0100

    Embed executable, expand fast memory and enable cache (slave v4)

--- StuntCarRacerSlave.s
+++ StuntCarRacerSlave.s
@@ -16,11 +16,12 @@
 	SUPER
 
 CHIPMEMSIZE	= $25000
-FASTMEMSIZE	= $9e000
+FASTMEMSIZE	= $120000
 NUMDRIVES	= 1
 WPDRIVES	= %0000
 HDINIT
 BOOTDOS
+CACHE
 
 slv_Version	= 16
 slv_Flags	= WHDLF_NoError|WHDLF_Examine
@@ -30,7 +31,7 @@ gameDataSize	equ	408416
 
 	INCLUDE	"kick31.s"
 
-	dc.b	"$VER: StuntCarRacer.slave 0.x (21.03.2026)",0
+		dc.b	"$VER: StuntCarRacer.slave 0.x (21.03.2026)",0
 
 slv_CurrentDir:	dc.b	0
 slv_name:	dc.b	"Stunt Car Racer",0
@@ -41,34 +42,49 @@ slv_info:	dc.b	"Adapted by Vesuri",10
 
 _bootdos:
 	move.l	_resload,a2
-	lea	_executable,a0
+
+	; Set up chip memory and kast memory pointers for _LoadSegFromBuffer
+	lea	_chipMem,a0
+	move.l	#$1000,(a0)
+	lea	_fastMem,a0
+	move.l	_expmem,d0
+	add.l	#KICKSIZE,d0
+	move.l	d0,(a0)
+
+	lea	executable,a0
 	jsr	_LoadSegFromBuffer
 	tst.l	d0
 	bne.s	.loadData
 
-.readFailure:
 	pea	TDREASON_WRONGVER
 	jmp	resload_Abort(a2)
 
 .loadData:
+	; _LoadSegFromBuffer returns a BPTR
 	add.l	d0,d0
 	add.l	d0,d0
 	addq	#4,d0
+
+	; Set beginning of code to A4
 	move.l	d0,a4
 
+	; Get gameData BSS address from a lea gameData,a0 instruction
 	move.l	8(a4),a5
+
+	; Rob Northen requires one longword before the actual payload
 	lea	-4(a5),a0
 	move.l	#$e898,d0
 	move.l	#gameDataSize+4,d1
 	moveq	#1,d2
+	move.l	_resload,a2
 	jsr	resload_DiskLoad(a2)
 
-	move.l	#(gameDataSize+4)>>2,d0
-	move.l	#$c905b365,d5
-	move.l	#$a0cff27b,d6
-	move.l	#$59f3a592,d7
-	lea	-4(a5),a0
-	bsr	_Decrypt
+;	move.l	#(gameDataSize+4)>>2,d0
+;	move.l	#$c905b365,d5
+;	move.l	#$a0cff27b,d6
+;	move.l	#$59f3a592,d7
+;	lea	-4(a5),a0
+;	bsr	_Decrypt
 
 	jsr	(a4)
 
@@ -97,8 +113,10 @@ _Decrypt	movem.l	d0/d5-d7/a0,-(sp)	;Rob Northen Decryption (3 Key)
 		movem.l	(sp)+,d0/d5-d7/a0
 		rts
 
-	INCLUDE	"LoadSegFromBuffer.s"
+		include	"LoadSegFromBuffer.s"
 
-_executable:	incbin	"StuntCarRacer"
+executable:	incbin	"StuntCarRacer"
+executableEnd:
+executableSize	equ	(executableEnd-executable)
 
 	END
