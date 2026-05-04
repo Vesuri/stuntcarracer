9a9f99ee183d55161617fae61be535580157d0c9
Vesa Halttunen <vesuri@jormas.com>
2026-05-04 20:11:43 +0200

    Restructure slave with debug area and loader refactor (slave v10)

--- StuntCarRacerSlave.s
+++ StuntCarRacerSlave.s
@@ -1,17 +1,10 @@
 ; Stunt Car Racer WHDLoad slave by Vesa Halttunen
-; TODO
-; - Load and save driver information to a separate file "StuntCarRacer.save"
-; - Trainer (F6 to toggle infinite boost, F7 to win the race)
-;   - Tooltype CUSTOM1=1 to automatically start with infinite boost
-; - Load and save fastest lap and track times
-; - Audio filter disable for clearer sound
-; - Fire button to bypass name entry sequence
-
 	incdir	"INCLUDE:"
 	include	"whdload.i"
 	include	"whdmacros.i"
 	include	"libraries/dos_lib.i"
 
+	IFD BARFLY
 	OUTPUT	"StuntCarRacer.slave"
 	BOPT	O+
 	BOPT	OG+
@@ -20,58 +13,106 @@
 	BOPT	w4-
 	BOPT	wo-
 	SUPER
-
-CHIPMEMSIZE	= $25000
-FASTMEMSIZE	= $a0000
-NUMDRIVES	= 1
-WPDRIVES	= %0000
-HDINIT
-BOOTDOS
-CACHE
-
-slv_Version	= 16
-slv_Flags	= WHDLF_NoError|WHDLF_Examine
-slv_keyexit	= $59	; F10
+	ENDC
 
 gameDataSize	equ	408540
+expMemSize	equ	$88000			; $87054
+debugBase	equ	$41800000
 
-		include	"kick31.s"
+;======================================================================
 
-		dc.b	"$VER: StuntCarRacer.slave 0.x (12.04.2026)",0
+_base		SLAVE_HEADER			;ws_Security + ws_ID
+		dc.w	17			;ws_Version
+		dc.w	WHDLF_EmulTrap|WHDLF_NoError	;ws_flags
+		dc.l	$80000			;ws_BaseMemSize
+		dc.l	0			;ws_ExecInstall
+		dc.w	_Start-_base		;ws_GameLoader
+		dc.w	0			;ws_CurrentDir
+		dc.w	0			;ws_DontCache
+_keydebug	dc.b	0			;ws_keydebug
+_keyexit	dc.b	$59			;ws_keyexit = F10
+_expmem		dc.l	expMemSize		;ws_ExpMem
+		dc.w	_name-_base		;ws_name
+		dc.w	_copy-_base		;ws_copy
+		dc.w	_info-_base		;ws_info
+		dc.w	0			;ws_kickname
+		dc.l	0			;ws_kicksize
+		dc.w	0			;ws_kickcrc
+		dc.w	.config-_base		;ws_config
 
-slv_CurrentDir:	dc.b	0
-slv_name:	dc.b	"Stunt Car Racer",0
-slv_copy:	dc.b	"1989 Geoff Crammond/Microstyle",0
-slv_info:	dc.b	"Adapted by Vesuri",10
-		dc.b	"Work in progress (12.04.2026)",0
-_PlayerName	dc.b	"Player      ",0
+.config		dc.b	"C1:B:Infinite Boost;"
+		dc.b	0
+
+;============================================================================
+
+_name		dc.b	"Stunt Car Racer",0
+_copy		dc.b	"1989 Geoff Crammond/Microstyle",0
+_info		dc.b	"Adapted by Vesuri",10
+		dc.b	"Work in progress "
+		IFD	BARFLY
+		IFND	.passchk
+		DOSCMD	"WDate >T:date"
+.passchk
+		ENDC
+		INCBIN	"T:date"
+		ELSE
+		dc.b	"(04.05.2026)"
+		ENDC
+		dc.b	-1,"This slave is partially based on the work by"
+		dc.b	10,"Codetapper/Action! & StingRay."
+		dc.b	-1,"Thanks to Harry, Carlo Pirri, AmiGer/CARE,"
+		dc.b	10,"Galahad and Frank for the great icons!"
+		dc.b	-1,"F6: Toggle infinite boost"
+		dc.b	10,"Help: Win race"
+		dc.b	0
 _TimesName	dc.b	"StuntCarRacer.times",0
 _SaveName	dc.b	"StuntCarRacer.save",0
-		even
+		dc.b	"$VER: StuntCarRacer.slave 0.x (04.05.2026)",0
+		EVEN
 
-_bootdos:
-	move.l	_resload,a2
+;======================================================================
+_Start						;A0 = resident loader
+;======================================================================
+	lea	_resload(pc),a1
+	move.l	a0,(a1)			;Save for later use
 
 	lea	executable,a0
-	jsr	_LoadSegFromBuffer
-	tst.l	d0
-	bne.s	.loadData
+	move.l	_expmem(pc),a1
+	move.l	a1,a5
+	move.l	#executableSize/4-1,d1
+.copy:	move.l	(a0)+,(a1)+
+	dbra	d1,.copy
 
-	pea	TDREASON_WRONGVER
-	jmp	resload_Abort(a2)
+	move.l	a5,d0
+	add.l	#expMemSize,d0
+	move.l	d0,sp
 
-.loadData:
-	; _LoadSegFromBuffer returns a BPTR
-	add.l	d0,d0
-	add.l	d0,d0
-	; Skip NextSeg
-	addq	#4,d0
+	move.l	_resload(pc),a2
+	move.l	a5,a0
+	clr.l	-(sp)
+	move.l	_ChipPtr(pc),-(sp)
+	pea     WHDLTAG_CHIPPTR
+	pea     -1
+	pea     WHDLTAG_LOADSEG
+	move.l  sp,a1
+	jsr	resload_Relocate(a2)
+	lea	5*4(sp),sp
 
-	; Set beginning of code to A4
-	move.l	d0,a4
+	; Get code base address (first hunk address + 8)
+	lea	8(a5),a4
 
-	; Get gameData BSS address from a lea gameData,a0 instruction
+	; Get gameData base address from a lea gameData,a0 instruction
 	move.l	2(a4),a5
+	move.l	a5,debugBase
+
+	; Store hook addresses
+	move.l	a5,a3
+	add.l	#gameDataSize,a3
+	lea	_SaveTimes(pc),a0
+	move.l	a0,(a3)+
+	lea	_Loader(pc),a0
+	move.l	a0,(a3)
+
 	; Rob Northen requires one longword before the actual payload
 	lea	-4(a5),a5
 
@@ -102,8 +143,16 @@ _bootdos:
 	move.l	#$59f3a592,d7
 	move.l	a5,a0
 	bsr	_Decrypt
-
 	jsr	resload_FlushCache(a2)
+
+	; Store save filesize
+	lea     _SaveName(pc),a0
+	jsr     resload_GetFileSize(a2)
+	lea     _SaveFileSize(pc),a0
+	move.l  d0,(a0)
+
+	; Disable filter
+	bset	#1,$bfe001
 
 	; Start the game
 	jsr	(a4)
@@ -113,72 +162,20 @@ _bootdos:
 	pea	TDREASON_OK
 	jmp	resload_Abort(a2)
 
-patchList:
-	PL_START
-;	PL_PS	$f036,_Keybd		;Detect quit key: ror.b #1,d0 and eori.b #$ff,d0
-;	PL_R	$59846			;Don't ask for floppy!
-;	PL_PS	$5b6bc,_SkipPlayerName	;Allow fire to skip name entry
-;	PL_L	$5b6c2,$4e714e71	;nop nop
-;	PL_P	$5f2f8,_SaveTimes	;move.b ($1c947).l,(2,a1,d1.w)
-;	PL_P	$62e86,_Loader		;Rob Northen loader
-	PL_END
-
+_resload	dc.l	0			;Resident loader
 _SaveFileSize	dc.l	0
+_ChipPtr	dc.l	$800
 
 ;======================================================================
 
-_WinRace	move.b	#4,($1bb20).l		;Cheating bastard! :)
-		rts
-
-;======================================================================
-
-_RefreshBoost	cmp.b	#0,$1ca20
-		bne.b	_ToggleBoost
-		move.b	#$80,($1ca20).l		;Lame trainer
-_ToggleBoost	;eor.l	#$65000008~$4e714e71,$60836
-		rts
-
-;======================================================================
-
-_SkipPlayerName	cmpi.b	#' ',(1,a0,d0.w)
-		bne.b	.NotBlankName
-		movem.l	d0/d1/a0/a1,-(sp)
-		movea.l	a0,a1
-		lea	_PlayerName(pc),a0
-		moveq	#11,d1
-.CopyPlayerName	move.b	(a0)+,(1,a1,d0.w)
-		addq.l	#1,d0
-		dbra	d1,.CopyPlayerName
-		movem.l	(sp)+,d0/d1/a0/a1
-.NotBlankName	rts
-
-;======================================================================
-
-_Keybd		ror.b	#1,d0			;Stolen 6 bytes
-		eor.b	#$ff,d0
-;		cmp.b	_keyexit(pc),d0
-;		beq.w	_exit
-		cmp.b	#$55,d0			;F6 = Refresh boost
-		beq.w	_RefreshBoost
-;		cmp.b	#$56,d0			;F7 = Switch turbo on/off
-;		beq.w	_ToggleTurbo
-		cmp.b	#$5f,d0			;Help = Win the race!
-		beq.w	_WinRace
-		rts
-
-;======================================================================
-
-_SaveTimes	move.b	($1c947).l,(2,a1,d1.w)	;Stolen code
-
-		movem.l	d0-d1/a0-a2,-(sp)
+_SaveTimes	movem.l	d0-d1/a0,-(sp)		;a1 = Address
 
 		lea	_TimesName(pc),a0	;a0 = Name
-		lea	$7a61a,a1		;a1 = Address
 		move.l	#$200,d0		;d0 = Size
 		move.l	_resload(pc),a2
 		jsr	resload_SaveFile(a2)
 
-		movem.l	(sp)+,d0-d1/a0-a2
+		movem.l	(sp)+,d0-d1/a0
 		rts
 
 ;======================================================================
@@ -188,8 +185,6 @@ _Loader		movem.l	d1-d4/a0-a3,-(sp)
                 move.l  _resload(pc),a2		;a0 = Address
 		mulu	#$200,d1		;d1 = Offset
 		mulu	#$200,d2		;d2 = Length
-		cmp.l	#$dc00,d1		;Initial game load
-		beq.b	_DiskLoad
 		cmp.l	#$a00,d1		;When game starts, it loads this
 		beq.b	_TimesLoad
 
@@ -206,6 +201,7 @@ _Loader		movem.l	d1-d4/a0-a3,-(sp)
 		cmp.l	d4,d3			;will work
 		blt.b	_DiskOpDone
 
+		move.l	a1,debugBase+4
 		jsr	resload_LoadFileOffset(a2)
 		bra.b	_DiskOpDone
 
@@ -216,16 +212,11 @@ _Save		jsr	resload_SaveFileOffset(a2)
 		move.l	d4,(a3)			;Save new file length
 		bra.b	_DiskOpDone
 
-_DiskLoad	move.l	d1,d0			;d0 = offset (bytes)
-		move.l	d2,d1			;d1 = length (bytes)
-		moveq	#1,d2			;d2 = disk
-		jsr	resload_DiskLoad(a2)	;a0 = destination
-
 _DiskOpDone	movem.l	(sp)+,d1-d4/a0-a3
 		moveq	#0,d0
 		rts
 
-_TimesLoad	move.l	a0,a1			;a1 = Source
+_TimesLoad	add.l	a0,a1			;a1 = Source
 		lea	_TimesName(pc),a0
 
 		movem.l	d0-d1/a0-a1,-(sp)
@@ -258,5 +249,5 @@ _Decrypt	movem.l	d0/d5-d7/a0,-(sp)	;Rob Northen Decryption (3 Key)
 		movem.l	(sp)+,d0/d5-d7/a0
 		rts
 
-		include	"LoadSegFromBuffer.s"
 executable:	incbin	"StuntCarRacer"
+executableSize	equ	*-executable
